02f094d36d4afb08373dd6a3515814fb03de172e
Jonathan Teo <jtmh@hotmail.com>
2020-01-10 22:56:58 +0800

    ✅ add tests for `ForexRates.convert()`
    - accepts common `Number` arguments
    - accepts `0` and negative quantities
    - self-conversion (EUR ⇨ EUR) uses the rate `1.0`

--- app/src/test/java/me/yonniton/forex/data/ForexRatesTest.kt
+++ app/src/test/java/me/yonniton/forex/data/ForexRatesTest.kt
@@ -5,8 +5,7 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import io.reactivex.Single
-import org.hamcrest.Matchers.equalTo
-import org.hamcrest.Matchers.hasSize
+import org.hamcrest.Matchers.*
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -41,5 +40,23 @@ class ForexRatesTest {
             assertValue { value -> "%tF".format(value.date) == "2018-09-06" }
             assertValue { value -> value.rates.size == 32 }
         }
+    }
+
+    @Test
+    fun `given a ForexRates instance, when a quantity of base currency is converted to any supported quote currency, then a calculated amount of quote currency should be returned`() {
+        val results = CurrencyCode.values()
+            .map { quote -> rates.convert(1, quote) }
+        assertThat("all supported currencies should be convertible", results.size, equalTo(CurrencyCode.values().size))
+    }
+
+    @Test
+    fun `given a ForexRates instance, when a quantity of base currency is converted to a quote currency, then the calculated amount of quote currency should be correct`() {
+        assertThat("EUR -100 should convert to AUD -162.07" , rates.convert(-100L, CurrencyCode.AUD), closeTo(-162.07, 1e-5))
+        assertThat("EUR -0.1 should convert to CNY -0.79663", rates.convert(-0.1f, CurrencyCode.CNY), closeTo(-0.79663, 1e-5))
+        assertThat("EUR  -50 should convert to EUR -50"     , rates.convert(  -50, CurrencyCode.EUR), equalTo(-50.0))
+        assertThat("EUR  0.2 should convert to GBP 0.180128", rates.convert(  0.2, CurrencyCode.GBP), closeTo(0.180128, 1e-5))
+        assertThat("EUR  100 should convert to JPY 12990.0" , rates.convert(  100, CurrencyCode.JPY), equalTo(12990.0))
+        assertThat("EUR   20 should convert to NZD 35.36"   , rates.convert(  20L, CurrencyCode.NZD), closeTo(35.36, 1e-5))
+        assertThat("EUR    0 should convert to USD 0.0"     , rates.convert(    0, CurrencyCode.USD), equalTo(0.0))
     }
 }
