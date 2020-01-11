ef0d408b3f6f9cd8d382df50c3c3e6c00be31b2e
Jonathan Teo <jtmh@hotmail.com>
2020-01-11 11:12:41 +0800

    ✨ query for forex-rates in `MainViewModel`
    - use `ON_RESUME` to fire API request
    - use Rx subscription-state to drive "in-progress" UI elements
    - populate `CurrenciesAdapter` on Rx-success

--- app/src/main/java/me/yonniton/forex/ui/main/MainViewModel.kt
+++ app/src/main/java/me/yonniton/forex/ui/main/MainViewModel.kt
@@ -1,9 +1,66 @@
 package me.yonniton.forex.ui.main
 
 import android.app.Application
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Toast
+import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import me.yonniton.forex.data.CurrencyCode
+import me.yonniton.forex.data.ForexRates
+import me.yonniton.forex.ui.CurrenciesAdapter
+import java.util.concurrent.TimeUnit
 
 class MainViewModel(application: Application) : LifecycleObserver, AndroidViewModel(application) {
-    // TODO: Implement the ViewModel
+
+    val progressVisibility = ObservableField<Int>(VISIBLE)
+    val ratesVisibility = ObservableField<Int>(GONE)
+    val ratesAdapter = ObservableField<CurrenciesAdapter>()
+
+    private val ratesQuery = ForexRates.ENDPOINT
+        .getForexRates(CurrencyCode.EUR)
+
+    private var disposable: Disposable? = null
+        set(value) {
+            field?.dispose()
+            field = value
+        }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private fun queryRates() {
+        ratesQuery
+            .timeout(5, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                // request success, populate forex-rates list
+                { ratesResponse ->
+                    progressVisibility.set(GONE)
+                    ratesVisibility.set(VISIBLE)
+                    ratesAdapter.set(CurrenciesAdapter(ratesResponse))
+                },
+                // request failure, blank screen with error Toast
+                {
+                    Toast.makeText(getApplication(), "could not retrieve forex rates", Toast.LENGTH_SHORT).show()
+                    progressVisibility.set(GONE)
+                    ratesVisibility.set(GONE)
+                }
+            )
+            .also {// request fired, show loading UI
+                disposable = it
+                progressVisibility.set(VISIBLE)
+                ratesVisibility.set(GONE)
+            }
+    }
+
+    override fun onCleared() {
+        disposable = null
+        super.onCleared()
+    }
 }
