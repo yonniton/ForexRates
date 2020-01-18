899699129add5235c94faebbcc40ad1215f88989
Jonathan Teo <jtmh@hotmail.com>
2020-01-18 09:57:20 +0800

    ✅ add tests for `MainViewModel`
    - 🤡 add Robolectric infrastructure for Android-mocking
    - 🤡 mock `ForexRates.Endpoint`
    - 🤡 add infrastructure for manipulating Rx schedulers
    - ✅ exercise `MainViewModel` lifecycle states

---
+++ app/src/test/java/me/yonniton/forex/ui/main/MainViewModelTest.kt
@@ -0,0 +1,81 @@
+package me.yonniton.forex.ui.main
+
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleRegistry
+import androidx.test.core.app.ApplicationProvider
+import com.google.gson.Gson
+import com.nhaarman.mockitokotlin2.*
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.TestScheduler
+import me.yonniton.forex.data.ForexRates
+import org.hamcrest.Matchers.*
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.io.IOException
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
+@RunWith(RobolectricTestRunner::class)
+class MainViewModelTest {
+
+    private lateinit var scheduler: TestScheduler
+    private lateinit var mockEndpoint: ForexRates.Endpoint
+    private lateinit var rates: ForexRates
+    private lateinit var lifecycleRegistry: LifecycleRegistry
+    private lateinit var viewmodel: MainViewModel
+
+    @Before
+    fun setUp() {
+        scheduler = TestScheduler()
+        RxJavaPlugins.setIoSchedulerHandler { scheduler }
+        RxJavaPlugins.setComputationSchedulerHandler { scheduler }
+        RxAndroidPlugins.setMainThreadSchedulerHandler {
+            object : Scheduler() {
+                override fun createWorker(): Scheduler.Worker {
+                    return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
+                }
+            }
+        }
+
+        rates = ClassLoader.getSystemResourceAsStream("rates.json")
+            .use { Gson().fromJson(it.reader(), ForexRates::class.java) }
+        viewmodel = MainViewModel(ApplicationProvider.getApplicationContext())
+        mockEndpoint = mock<ForexRates.Endpoint> {
+            on { getForexRates(any()) } doReturn Single.just(rates)
+        }.also { viewmodel.endpoint = it }
+        lifecycleRegistry = LifecycleRegistry(mock())
+            .also { it.addObserver(viewmodel) }
+    }
+
+    @Test
+    fun `when MainViewModel is instantiated, then it should be in the loading state, and it should have no ForexRates`() {
+        with(viewmodel) {
+            assertThat("the progress-spinner should be shown", progressVisibility.get(), equalTo(VISIBLE))
+            assertThat("the rates-list should be hidden", ratesVisibility.get(), equalTo(GONE))
+            assertThat("there should be no rates-response", rates, nullValue())
+            assertThat("there should be no rates-adapter", ratesAdapter.get(), nullValue())
+        }
+        verify(mockEndpoint, never()).getForexRates(any())
+    }
+
+    @Test
+    fun `when MainViewModel resumes, then it should be in the loading state, and it should have started a ForexRates query`() {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        with(viewmodel) {
+            assertThat("the progress-spinner should be shown", progressVisibility.get(), equalTo(VISIBLE))
+            assertThat("the rates-list should be hidden", ratesVisibility.get(), equalTo(GONE))
+            assertThat("there should be no rates-response", rates, nullValue())
+            assertThat("there should be no rates-adapter", ratesAdapter.get(), nullValue())
+        }
+        verify(mockEndpoint, only()).getForexRates(viewmodel.baseCurrency)
+    }
+}
